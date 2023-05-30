2ea11ffd9f87a9ea28b7d9b794c142f058cad2c9
wbandersonjr <wbandersonjr@aol.com>
2023-05-30 13:33:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ validateEntries = function (which) {
     for (let i = 0; i < Grades.length-1; i++) {
         let elem = document.getElementById(Grades[i][0]).value;
         let regex = /^(\d*)(\.?)(\d+)$/;
-        if (regex.test(elem) && elem>0 && elem<100.01) {
+        if (regex.test(elem) && elem>0 && elem<100.001) {
             let target = "error_";
             target = target.concat(Grades[i][0]);
             document.getElementById(target).style.display = "none";
@@ -34,7 +34,7 @@ calcFinal = function (which) {
         console.log(document.getElementById(Grades[i][0]).value);
         console.log(Grades[i][1]);
         culmGr += document.getElementById(Grades[i][0]).value*Grades[i][1];
-        console.log(culmGr);
+        console.log("culmGr = " + culmGr);
 
     } 
 
@@ -48,32 +48,33 @@ calcFinal = function (which) {
             }
             for (let i = Grades.length-3; i < Grades.length-1; i++) {
                 grCurr += document.getElementById(Grades[i][0]).value*0.25;
-                console.log(grCurr);
+                console.log("grCurr = " + grCurr);
             };
             break;
         case 2:
             console.log("semester calc");
             for (let i = 0; i < Grades.length-1; i++) {
                 grCurr += document.getElementById(Grades[i][0]).value*0.5;
-                console.log(grCurr);
+                console.log("grCurr = " + grCurr);
             };
             break;
     };
+    grCurr = grCurr.toFixed(2);
 
     let val = Number(document.getElementById("goal").value);
     let lastIndex = Grades.length-1;
     console.log(val);
     let lowest = "F";
 
-    if (culmGr >= 89.6) {
+    if (culmGr >= 89.5) {
         lowest = "an A";
-    } else if (culmGr >= 79.6) {
+    } else if (culmGr >= 79.5) {
         lowest = "a B";
-    } else if (culmGr >= 69.6) {
+    } else if (culmGr >= 69.5) {
         lowest = "a C";
-    } else if (culmGr >= 59.6) {
+    } else if (culmGr >= 59.5) {
         lowest = "a D";
-    } else if (culmGr >= 39.6) {
+    } else if (culmGr >= 39.5) {
         lowest = "an E";
     };
     switch (val) {
@@ -109,6 +110,25 @@ calcFinal = function (which) {
 
     };
 };
+
+changeExempt = function () {
+    if (document.getElementById("se1_exempt").checked) {
+        document.getElementById("se1").disabled=true;
+        document.getElementById("se1").value="";
+        document.getElementById("error_se1").style.display="none";
+        Grades[0][1]=0.25;
+        Grades[1][1]=0.25;
+        Grades.splice(2,1);
+        console.log(Grades);
+    } else {
+        document.getElementById("se1").disabled=false;
+        Grades[0][1]=0.2;
+        Grades[1][1]=0.2;
+        Grades.splice(2,0,["se1",0.1]);
+        console.log(Grades);
+    };
+};
+
 
 
 
